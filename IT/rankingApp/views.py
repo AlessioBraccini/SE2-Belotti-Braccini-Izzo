a5e076b0cac90ce53a956910053631fa4a09a026
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-12-29 22:09:45 +0100

    update get_ranking

--- IT/rankingApp/views.py
+++ IT/rankingApp/views.py
@@ -4,24 +4,33 @@ from rest_framework.views import APIView
 from rest_framework import authentication, permissions
 from django.contrib.auth import get_user_model
 from app.models import Farm
+import json
 
 User = get_user_model()
 
 
 # Create your views here.
 
+class Ranking(object):
+    name = None
+    score = None
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
 
 def get_ranking(district, ordering):
-    mydict = {}
+    mylist = []
     if ordering == 'descending':
         farms = Farm.objects.filter(user_id__district=district).order_by('-score')
     else:
         farms = Farm.objects.filter(user_id__district=district).order_by('score')
     if farms.count() > 0:
         for f in farms:
-            mydict[f.user_id.complete_name()] = f.score
-    # sorted_list = sorted_list[0:10]  # top/worst 10
-    return mydict
+            rank = Ranking(f.user_id.complete_name(), f.score)
+            mylist.append(rank)
+    return mylist
 
 
 class RankFarmers(APIView):
@@ -34,16 +43,15 @@ class RankFarmers(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request):
+    @staticmethod
+    def get(request):
         """
         Return a list of all users.
         """
         user = User.objects.get(email=request.user.email)
         if user is None:
             raise Http404("User not found")
-        print(user.district)
         ordering = request.GET.get('ordering')
-        json_string = get_ranking(user.district, ordering)
-        return JsonResponse(json_string)
-
-
+        mylist = get_ranking(user.district, ordering)
+        json_string = json.dumps(mylist, default=lambda x: x.__dict__)
+        return JsonResponse(json_string, safe=False)
