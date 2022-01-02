8c12d43a45730e8bfa5929e8fc5db094e391fa9a
Ottavia <ottavia.belotti@mail.polimi.it>
2022-01-02 20:08:13 +0100

    Fix: parsing DailyPlanView "visit_farmers_list" field in post() now gets a list. Removing "annotation field" in code, not in db yet

--- IT/planningApp/views.py
+++ IT/planningApp/views.py
@@ -8,16 +8,11 @@ from rest_framework import authentication, permissions
 from django.contrib.auth import get_user_model
 from .models import *
 from app.models import Farm
-import json
 
 User = get_user_model()
 
 # Create your views here.
-# DONE create a new daily plan -> date, list of farms
-# return list of farms to choose from -> farm and last visit/counter
-# modify existing daily plan -> add and remove farm
-# DONE return daily plan
-# add annotations
+# todo: remove annotation from db and methods
 
 
 class DailyPlanView(APIView):
@@ -27,9 +22,12 @@ class DailyPlanView(APIView):
     @staticmethod
     def post(request):
         agro = User.objects.get(id=request.user.id)
-        farmers_ids = request.data['visit_farmers_list']
-        farmers_ids = list(farmers_ids.split(","))
+        if agro.job_role != "A":
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        farmers_ids = request.POST.getlist('visit_farmers_list')
         date = request.data['date']
+        if DailyPlan.objects.filter(agronomist_user=agro, date=date).count() > 0:
+            return Response({"message": "This user already have a plan for this date"}, status=status.HTTP_403_FORBIDDEN)
         # todo: normalize date to be always in format YYYY-MM-DD
         daily_plans = []
         for farmer_id in farmers_ids:
@@ -45,7 +43,7 @@ class DailyPlanView(APIView):
             farm.visit_ctr += 1
             farm.save()  # update visit counter farm
             plan.save()
-        return Response({"message:" "New daily plan saved successfully."})
+        return Response({"message": "New daily plan saved successfully."})
 
     @staticmethod
     def get(request):
@@ -74,7 +72,6 @@ class UpdateVisits(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     @staticmethod
-    # @api_view(['POST', 'PUT'])
     def post(request):
         agro = User.objects.get(id=request.user.id)
         try:
@@ -82,10 +79,12 @@ class UpdateVisits(APIView):
         except User.DoesNotExist:
             return Response({"message": "Farmer not found"}, status=status.HTTP_404_NOT_FOUND)
         date = request.data["date"]
+        '''
         try:
             annotation = request.data["annotation"]
         except MultiValueDictKeyError:
             annotation = None
+        '''
 
         # either add, delete, add_annotation
         action = request.GET.get('action')
@@ -97,22 +96,20 @@ class UpdateVisits(APIView):
             farm.save()  # update visit counter farm
             plan_entry.save()
             return Response({"message": "New visit registered successfully"})
+        '''
         if action == 'add_annotation':
             if annotation is not None:
                 plan_entry = DailyPlan.objects.get(agronomist_user=agro, date=date, visit_farmer=farmer)
                 plan_entry.annotation = annotation
                 plan_entry.save()
             return Response({"message": "Daily plan entry updated successfully"})
+        '''
         if action == 'delete':
             plan_entry = DailyPlan.objects.get(agronomist_user=agro, date=date, visit_farmer=farmer)
             farm = Farm.objects.get(user=plan_entry.visit_farmer)
             farm.visit_ctr -= 1
             farm.save()  # update visit counter farm
             DailyPlan.delete(plan_entry)
-            return Response({"message" : "Visit deleted successfully"})
+            return Response({"message": "Visit deleted successfully"})
         return Response({"message": "Action is not a valid one. Choose between add, add_annotation or delete"},
                             status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
