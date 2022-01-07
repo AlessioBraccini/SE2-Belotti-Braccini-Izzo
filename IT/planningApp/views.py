75688213a9287a974009bc377d309faec4855bdb
Ottavia <ottavia.belotti@mail.polimi.it>
2022-01-07 13:24:04 +0100

    Add: get all dailyPlans dates And get() for dailyPlan by date has been moved to UpdateVisits class

--- IT/planningApp/views.py
+++ IT/planningApp/views.py
@@ -22,9 +22,6 @@ class DailyPlanView(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
-
-
-
     @staticmethod
     def post(request):
         agro = User.objects.get(id=request.user.id)
@@ -53,28 +50,20 @@ class DailyPlanView(APIView):
 
     @staticmethod
     def get(request):
+        """
+        :param request:
+        :return: list of date for which the agronomist has a daily plan
+        """
         agro = User.objects.get(id=request.user.id)
-        try:
-            plans = DailyPlan.objects.filter(agronomist_user__id=agro.id, date=request.GET.get('date'))
-        except DailyPlan.DoesNotExist:
-            return Response({"message": "daily plan not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        except ValidationError:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        if agro.job_role != "A":
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
-        farmers_list = []
-        for plan in plans:
-            context = {
-                'farmer_id': plan.visit_farmer.id,
-                'farmer_name': plan.visit_farmer.complete_name(),
-            }
-            farmers_list.append(context)
+        plan_entries = DailyPlan.objects.filter(agronomist_user=agro).values('date').distinct()
+        print(str(plan_entries))
+        formatted_data = json.dumps(el.__dict__ for el in plan_entries)
 
-        response_payload = {
-            'date': request.GET.get('date'),
-            'visit_farmers_list': farmers_list,
-        }
-        return Response(response_payload)
+        return Response(formatted_data)
 
 
 class UpdateVisits(APIView):
@@ -95,7 +84,34 @@ class UpdateVisits(APIView):
         # make the new insertion
         response = DailyPlanView.post(request)
 
-        if response.data['message'] == 'New daily plan saved successfully.':
+        if response['message'] == 'New daily plan saved successfully.':
             return Response({"message": "Daily plan for date " + date + " has been updated successfully."})
         # todo: if error, reload the old_plan_entries
         return response
+
+    @staticmethod
+    def get(request):
+        """
+        Return a list of the farms in the current daily plan
+        :param request: request that specify the date of the daily plan the user wants to know of
+        :return: list of farms in the daily plan
+        """
+        agro = User.objects.get(id=request.user.id)
+        farmers_list = []
+        try:
+            plans = DailyPlan.objects.filter(agronomist_user__id=agro.id, date=request.GET.get('date'))
+        except DailyPlan.DoesNotExist:
+            return Response({"message": "daily plan not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        except ValidationError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        for plan in plans:
+            visit_ctr = Farm.objects.get(user=plan.visit_farmer).first().visit_ctr
+            farmers_list.append((plan.visit_farmer.id, plan.visit_farmer.complete_name(), visit_ctr, plan.annotation))
+
+        response_payload = {
+            'date': request.GET.get('date'),
+            'visit_farmers_list': farmers_list,
+        }
+        return Response(response_payload)
