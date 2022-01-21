9ba8186cfdcbb20659b3d99483eee37618d31dfe
Ottavia <ottavia.belotti@mail.polimi.it>
2022-01-21 14:00:33 +0100

    Update: backend debug disabled

--- IT/dream_backend/settings.py
+++ IT/dream_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=(f_+4l*td*=chaj$$vpr*_)^zapbfpvg_lsgxp0m6+kh6b(8r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
